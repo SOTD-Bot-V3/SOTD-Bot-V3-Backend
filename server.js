500e56cf4f8807f48e6028e441f46b80ad58c412
ComputerComa <walkerraydick2@gmail.com>
2023-07-25 15:16:06 -0500

    working on API

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const config = require('./config.json');
+const fetch = require('isomorphic-unfetch');
+const { getData } = require('spotify-url-info')(fetch);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -9,4 +11,18 @@ app.get('/', (req, res) => {
 app.listen(config.PORT, () => {
 	console.info(`Listening on http://localhost:${config.PORT}`);
 });
-app.get('/lookup/')
+app.get('/song/lookup/:id', async (req, res, next) => {
+	try {
+		let data;
+		await getData(`https://open.spotify.com/track/${req.params['id']}`).then(outdata => {
+			data = outdata;
+			console.log(data);
+			res.json(data);
+		});
+	}
+	catch (error) {
+		console.error(error);
+		return next(error);
+	}
+
+});
